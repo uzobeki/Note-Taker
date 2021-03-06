f9025cac58dc9a373c52bdcb20b6c91721903ebb
Zo Beki <uobeki.40@gmail.com>
2021-03-06 18:13:07 -0500

    route.js additions

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
